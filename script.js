92a6c973f527e91e1e0831b9e8927d5952f29213
Lemuller04 <troublessleepy@gmail.com>
2024-05-19 12:31:14 -0300

    Add getHumanInput and formatUserInput functions

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-let CHOICES = ["Rock", "Paper", "Scissor"]
+let CHOICES = ["Rock", "Paper", "Scissor"];
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
-    return CHOICES[Math.floor(Math.random() * CHOICES.length)]
+    return CHOICES[Math.floor(Math.random() * CHOICES.length)];
+}
+
+function getHumanChoice() {
+    let userInputIsValid = 0;
+    let userInputFormatted = "";
+
+    // Prompts the user for a input until he enters a valid one
+    while (!userInputIsValid) {
+        let userInput = prompt("Rock, paper or scissors?");
+
+        userInputFormatted = formatUserInput(userInput);
+
+        // Compares formatted user input to every valid choice
+        for (let i = 0; i < CHOICES.length; i++) {
+
+            // If formatted user input matches one valid choice, breaks the loop
+            if (userInputFormatted === CHOICES[i]) {
+                userInputIsValid=1;
+            }
+        }
+    }
+
+    return userInputFormatted;
+}
+
+// Returns the user input trimmed and capitalized
+function formatUserInput(input) {
+    return (input.trim().charAt(0).toUpperCase() + input.trim().slice(1).toLowerCase()).trim();
 }
